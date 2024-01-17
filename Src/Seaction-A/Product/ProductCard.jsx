76ac1c46c96870875bed19dc5a61be10d04b5f6d
akshat <akshatbhatya@gmail.com>
2024-01-18 00:35:44 +0530

    optimize array destructring

--- Src/Seaction-A/Product/ProductCard.jsx
+++ Src/Seaction-A/Product/ProductCard.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import "./Product.css"
 
 function ProductCard({ data }) {
+  let {name,areaName,cuisines,avgRating,cloudinaryImageId}=data?.info;
+  let {deliveryTime}=data?.info.sla
   return (
     <div className="card-container">
 
       <div className='card'  >
-        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/" + data.info.cloudinaryImageId} alt="food" />
-        <h3>{data.info.name}</h3>
-        <h4>{data.info.areaName}</h4>
-        <h5>{data.info.cuisines}</h5>
-        <h4>{data.info.avgRating}</h4>
-        <h4>{data.info.sla.deliveryTime} min</h4>
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/" + cloudinaryImageId} alt="food" />
+        <h3>{name}</h3>
+        <h4>{areaName}</h4>
+        <h5>{cuisines.join(", ")}</h5>
+        <h4>{avgRating}</h4>
+        <h4>{deliveryTime} min</h4>
       </div>
     </div>
   )
